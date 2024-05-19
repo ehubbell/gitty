9ff7b1fcffa7ae9c7f6497bc730195df608a78ed
Eric Hubbell <eric@erichubbell.com>
2024-05-19 08:21:53 -0600

    Transfer rename

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,17 @@ const sade = require('sade');
 import { fetchCommand } from 'src/commands/fetch';
 import { version } from '../package.json';
 
-const cli = sade('gitdl <url>', true)
+const cli = sade('transfer <url>', true)
 	.version(version)
-	.command('fetch <url>')
-	.describe('Fetch a Github repository or subdirectory and clone it to your Github.')
+	.describe('Fetch a Github repository or subdirectory and transfer it to your Github.')
 	.option('-c, --clone', 'Perform clone after download is complete.')
 	.option('-d, --destination', 'Path to destination directory.')
 	.option('-t, --token', 'Add your token')
 	.option('-v, --version', 'Specify tarball version (optional).')
-	.example('gitdl vercel/vercel')
-	.example('gitdl vercel/vercel/examples/angular')
-	.example('gitdl vercel/vercel/examples/angular --directory ~/storage')
-	.example('gitdl vercel/vercel/examples/angular --directory ~/storage --clone playbooks-community')
+	.example('transfer vercel/vercel')
+	.example('transfer vercel/vercel/examples/angular')
+	.example('transfer vercel/vercel/examples/angular --directory ~/storage')
+	.example('transfer vercel/vercel/examples/angular --directory ~/storage --clone playbooks-community')
 	.action(fetchCommand);
 
 cli.parse(process.argv);
