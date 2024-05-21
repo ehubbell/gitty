d73b4326bffbf60498013c2d2b0639c7a228828f
Eric Hubbell <eric@erichubbell.com>
2024-05-21 16:14:36 -0600

    Include license

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,17 @@ const sade = require('sade');
 import { fetchCommand } from 'src/commands/fetch';
 import { version } from '../package.json';
 
-const cli = sade('transfer <url>', true)
+const cli = sade('playbooks-transfer <url>', true)
 	.version(version)
 	.describe('Fetch a Github repository or subdirectory and transfer it to your Github.')
 	.option('-c, --clone', 'Perform clone after download is complete.')
 	.option('-d, --destination', 'Path to destination directory.')
 	.option('-t, --token', 'Specify your github token.')
 	.option('-v, --version', 'Specify tarball version (optional).')
-	.example('transfer vercel/vercel')
-	.example('transfer vercel/vercel/examples/angular')
-	.example('transfer vercel/vercel/examples/angular --directory ~/storage')
-	.example('transfer vercel/vercel/examples/angular --directory ~/storage --clone playbooks-community')
+	.example('playbooks-transfer vercel/vercel')
+	.example('playbooks-transfer vercel/vercel/examples/angular')
+	.example('playbooks-transfer vercel/vercel/examples/angular --directory ~/storage')
+	.example('playbooks-transfer vercel/vercel/examples/angular --directory ~/storage --clone playbooks-community')
 	.action(fetchCommand);
 
 cli.parse(process.argv);
--- src/services/storage-service.ts
+++ src/services/storage-service.ts
@@ -1,125 +1,116 @@
-const Archiver = require("archiver");
-const Fs = require("fs-extra");
-const Stream = require("fstream");
-const Unzip = require("unzip-stream");
-import * as FileSystem from "src/utils/file-system";
+const Archiver = require('archiver');
+const Fs = require('fs-extra');
+const Stream = require('fstream');
+const Unzip = require('unzip-stream');
+import * as FileSystem from 'src/utils/file-system';
 
 interface StorageService {
-  basePath: string;
-  ownerId: string;
-  repoId: string;
-  nestedPath?: string;
+	basePath: string;
+	ownerId: string;
+	repoId: string;
+	nestedPath?: string;
 }
 
 class StorageService {
-  constructor(props: {
-    basePath: string;
-    ownerId: string;
-    repoId: string;
-    nestedPath?: string;
-  }) {
-    this.basePath = props.basePath;
-    this.ownerId = props?.ownerId;
-    this.repoId = props?.repoId;
-    this.nestedPath = props?.nestedPath;
-  }
+	constructor(props: { basePath: string; ownerId: string; repoId: string; nestedPath?: string }) {
+		this.basePath = props.basePath;
+		this.ownerId = props?.ownerId;
+		this.repoId = props?.repoId;
+		this.nestedPath = props?.nestedPath;
+	}
 
-  /* ----- Computed ----- */
-  get zipFile() {
-    return `${this.basePath}/${this.repoId}.zip`;
-  }
+	/* ----- Computed ----- */
+	get zipFile() {
+		return `${this.basePath}/${this.repoId}.zip`;
+	}
 
-  /* ----- Methods ----- */
-  async checkEmpty() {
-    // console.log("Checking path: ", this.basePath);
-    const pathExists = await FileSystem.checkPath(this.basePath);
-    if (pathExists) {
-      const path = await Fs.promises.stat(this.basePath);
-      if (path.isDirectory()) {
-        const entries = await Fs.readdir(this.basePath);
-        return entries.length > 0 ? false : true;
-      }
-      return false;
-    }
-    return true;
-  }
+	/* ----- Methods ----- */
+	async checkEmpty() {
+		// console.log("Checking path: ", this.basePath);
+		const pathExists = await FileSystem.checkPath(this.basePath);
+		if (pathExists) {
+			const path = await Fs.promises.stat(this.basePath);
+			if (path.isDirectory()) {
+				const entries = await Fs.readdir(this.basePath);
+				return entries.length > 0 ? false : true;
+			}
+			return false;
+		}
+		return true;
+	}
 
-  async saveRepo(buffer: ArrayBuffer) {
-    // console.log("Saving repo...");
-    await FileSystem.checkOrCreatePath(this.basePath);
-    await FileSystem.writeFile(this.zipFile, Buffer.from(buffer));
-    // console.log("repo saved.");
-  }
+	async saveRepo(buffer: ArrayBuffer) {
+		// console.log("Saving repo...");
+		await FileSystem.checkOrCreatePath(this.basePath);
+		await FileSystem.writeFile(this.zipFile, Buffer.from(buffer));
+		// console.log("repo saved.");
+	}
 
-  async fetchRepoStats() {
-    // console.log("fetching stats: ", this.basePath);
-    return await FileSystem.fileStats(this.basePath);
-  }
+	async fetchRepoStats() {
+		// console.log("fetching stats: ", this.basePath);
+		return await FileSystem.fileStats(this.basePath);
+	}
 
-  async unzipRepo() {
-    // console.log("Unzipping repo: ", this.zipFile);
-    await FileSystem.checkOrCreatePath(this.basePath);
+	async unzipRepo() {
+		// console.log("Unzipping repo: ", this.zipFile);
+		await FileSystem.checkOrCreatePath(this.basePath);
 
-    await new Promise((resolve, reject) => {
-      Fs.createReadStream(this.zipFile)
-        .pipe(Unzip.Parse())
-        .on("entry", (entry: any) => {
-          const type = entry.type;
-          const fileName = entry.path;
-          const slicedPathName = fileName.slice(
-            fileName.indexOf("/"),
-            fileName.length
-          );
-          const formattedPath = slicedPathName.replace(
-            "/" + this.nestedPath,
-            "/"
-          );
-          const finalPath = this.basePath + formattedPath;
-          if (type === "Directory") return entry.autodrain();
-          if (this.nestedPath) {
-            if (fileName.includes(this.nestedPath)) {
-              const writer = Stream.Writer({ path: finalPath });
-              return entry.pipe(writer);
-            }
-            return entry.autodrain();
-          } else {
-            const writer = Stream.Writer({ path: finalPath });
-            return entry.pipe(writer);
-          }
-        })
-        .on("close", (v) => resolve(v))
-        .on("error", (e) => reject(e));
-    });
-  }
+		await new Promise((resolve, reject) => {
+			Fs.createReadStream(this.zipFile)
+				.pipe(Unzip.Parse())
+				.on('entry', (entry: any) => {
+					const type = entry.type;
+					const fileName = entry.path;
+					const slicedPath = fileName.slice(fileName.indexOf('/'), fileName.length);
+					const formattedPath = slicedPath.replace('/' + this.nestedPath, '/');
+					const finalPath = this.basePath + formattedPath;
+					if (type === 'Directory') return entry.autodrain();
+					if (this.nestedPath) {
+						const rootPath = slicedPath.split('/')[1];
+						const rootFile = rootPath.toLowerCase();
+						if (fileName.includes(this.nestedPath) || rootFile === 'license.md') {
+							const writer = Stream.Writer({ path: finalPath });
+							return entry.pipe(writer);
+						}
+						return entry.autodrain();
+					} else {
+						const writer = Stream.Writer({ path: finalPath });
+						return entry.pipe(writer);
+					}
+				})
+				.on('close', v => resolve(v))
+				.on('error', e => reject(e));
+		});
+	}
 
-  async cleanRepo() {
-    await FileSystem.removePath(this.basePath + "/.git");
-    await FileSystem.removePath(this.basePath + "/.github");
-  }
+	async cleanRepo() {
+		await FileSystem.removePath(this.basePath + '/.git');
+		await FileSystem.removePath(this.basePath + '/.github');
+	}
 
-  async zipRepo() {
-    // console.log("Zipping repo: ", this.basePath);
-    await FileSystem.checkOrCreatePath(this.basePath);
-    const archive = Archiver("zip", { zlib: { level: 9 } });
-    const stream = Fs.createWriteStream(this.zipFile);
+	async zipRepo() {
+		// console.log("Zipping repo: ", this.basePath);
+		await FileSystem.checkOrCreatePath(this.basePath);
+		const archive = Archiver('zip', { zlib: { level: 9 } });
+		const stream = Fs.createWriteStream(this.zipFile);
 
-    await new Promise((resolve, reject) => {
-      stream.on("close", (v) => resolve(v));
-      archive.directory(this.basePath, false);
-      archive.on("error", (err) => reject(err));
-      archive.pipe(stream);
-      archive.finalize();
-    });
-  }
+		await new Promise((resolve, reject) => {
+			stream.on('close', v => resolve(v));
+			archive.directory(this.basePath, false);
+			archive.on('error', err => reject(err));
+			archive.pipe(stream);
+			archive.finalize();
+		});
+	}
 
-  async removeRepo() {
-    await FileSystem.removePath(this.basePath);
-  }
+	async removeRepo() {
+		await FileSystem.removePath(this.basePath);
+	}
 
-  async removeZip() {
-    // console.log("Removing zip...");
-    await FileSystem.removePath(this.zipFile);
-  }
+	async removeZip() {
+		// console.log("Removing zip...");
+		await FileSystem.removePath(this.zipFile);
+	}
 }
 
 export { StorageService };
